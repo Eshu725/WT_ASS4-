a513a4a4b8b2f21d97961a1e28c78b125a2fec92
lifetheory <66261091+Eshu725@users.noreply.github.com>
2020-11-01 13:39:05 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+  
+  <title>ass4</title>
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+  <nav class="navbarNav" class="navbar navbar-expand-md navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">INSANE BITES</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+   
+  </nav>
+
+  <div class="container">
+    <div class="mt-5 text-center">
+      <h2>Welcome to the Restaurant!!</h2>
+      
+    </div>
+    <div class="mt-4 text-center">
+      <select id="menudrop" class="form-control">
+        <option selected>Please select from below menu</option>
+      </select>
+    </div>
+    <div class="mt-5 row">
+      <div class="card col-md-8 offset-md-2" style="width: 18rem;">
+        <div class="card-body" id="dataholder">
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src=" https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+    crossorigin="anonymous"></script>
+  <script src="index.js"></script>
+
+  <footer style="background-color: #4F4846; padding: 20px;">
+    <p style="text-align: center; color: white; font-size: 20px">Waiting for your delicious feedbacks!!!
+    mail:insanebites@gmail.com</p>
+  </footer>
+</body>
+
+</html>
